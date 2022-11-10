df189093befe28497e21480d1591c0c70b4836e4
miaslls <97785569+miaslls@users.noreply.github.com>
2022-11-10 19:31:11 -0300

    fix User, UpdateUserDto

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,5 +1,4 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { PartialType } from '@nestjs/swagger';
 import { CreateUserDto } from './create-user.dto';
 
-// FIXME: hate this, do it properly
 export class UpdateUserDto extends PartialType(CreateUserDto) {}
