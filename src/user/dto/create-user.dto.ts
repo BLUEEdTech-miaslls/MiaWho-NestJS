3b58ba374a8d28880135fd7ff7e5b7cd0d6bb24e
miaslls <97785569+miaslls@users.noreply.github.com>
2022-11-10 19:30:28 -0300

    CreateUserDto validate

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,20 +1,26 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsString, MinLength } from 'class-validator';
 
 export class CreateUserDto {
+  @IsString()
   @ApiProperty({
     description: 'full name',
     example: 'Jane Doe',
   })
   name: string;
+
+  @IsEmail()
   @ApiProperty({
     description: 'email address',
     example: 'janedoe@miamail.com',
   })
   email: string;
+
+  @IsString()
+  @MinLength(8)
   @ApiProperty({
-    description:
-      'min 8 chars, capital letter, lowercase letter, number, special character',
-    example: '123456Aa!',
+    description: 'min 8 chars',
+    example: '123456A78',
   })
   password: string;
 }
