38301c31dd7886f2943a91ccaf7ce81c189d4b15
miaslls <97785569+miaslls@users.noreply.github.com>
2022-11-10 11:50:30 -0300

    FIX ME:

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,5 +1,5 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
 
-// TODO: hate this, do it properly
+// FIXME: hate this, do it properly
 export class UpdateUserDto extends PartialType(CreateUserDto) {}
