7002d658c92500e4dbe61ab9c8c6d2fc909a4242
miaslls <97785569+miaslls@users.noreply.github.com>
2022-11-10 20:38:04 -0300

    auth

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,34 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
+
+import { LoginDto } from './dto/login.dto';
+import { LoginResponseDto } from './dto/login-response.dto';
+
+@Injectable()
+export class AuthService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async login(dto: LoginDto): Promise<LoginResponseDto> {
+    const { email, password } = dto;
+
+    const user = await this.prisma.user.findUnique({ where: { email } });
+
+    if (!user) {
+      throw new UnauthorizedException('invalid user/password');
+    }
+
+    const isHashValid = await bcrypt.compare(password, user.password);
+
+    if (!isHashValid) {
+      throw new UnauthorizedException('invalid user/password');
+    }
+
+    delete user.password;
+
+    return {
+      token: 'test',
+      user,
+    };
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,9 @@ async function bootstrap() {
     .setDescription('User Manager')
     .setVersion('1.0.0')
     .addTag('status')
+    .addTag('auth')
     .addTag('user')
+    .addBearerAuth()
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
